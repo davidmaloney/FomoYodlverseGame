bc3bbadf72a1c4ec57adc99af77aea0af0f9d92b
davidmaloney <lesabeillesduvallon@gmail.com>
2025-12-05 14:28:54 +0100

    Update StartHandler.java

--- src/main/java/com/yourname/bot/handlers/StartHandler.java
+++ src/main/java/com/yourname/bot/handlers/StartHandler.java
@@ -1,1 +1,29 @@
+package com.yourname.bot.handlers;
 
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+
+public class StartHandler {
+
+    // Called by MasterHandler when /start is received
+    public void handleStart(Update update, AbsSender sender) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            String chatId = message.getChatId().toString();
+            String welcomeText = "🚀 Welcome to the FOMO YODLverse! " +
+                    "Your adventure starts now. Use the buttons to explore, battle, and collect loot!";
+
+            SendMessage response = new SendMessage();
+            response.setChatId(chatId);
+            response.setText(welcomeText);
+
+            try {
+                sender.execute(response);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
