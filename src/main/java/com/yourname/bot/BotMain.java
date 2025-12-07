079d9981ef237ab9fd7519f19142600374d56c6f
davidmaloney <lesabeillesduvallon@gmail.com>
2025-12-07 19:16:59 +0100

    Update BotMain.java

--- src/main/java/com/yourname/bot/BotMain.java
+++ src/main/java/com/yourname/bot/BotMain.java
@@ -1,27 +1,34 @@
-package com.formalyodelversegame.bot;
+package com.yourname.bot;
 
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import com.formalyodelversegame.bot.handlers.MasterHandler;
-import com.formalyodelversegame.bot.handlers.StartHandler;
+
+import com.yourname.bot.handlers.HandlerRouter;
 
 public class BotMain extends TelegramWebhookBot {
 
-    private final String botUsername;
     private final String botToken;
-    private final String botPath;
+    private final String botUsername;
+    private final String webhookPath;
 
-    private final MasterHandler masterHandler;
-    private final StartHandler startHandler;
+    private final HandlerRouter handlerRouter;
 
-    public BotMain() {
-        this.botUsername = System.getenv("TELEGRAM_BOT_USERNAME");
-        this.botToken = System.getenv("TELEGRAM_BOT_TOKEN");
-        this.botPath = System.getenv("TELEGRAM_BOT_WEBHOOK");
+    public BotMain(String botToken, String botUsername, String webhookPath) {
+        this.botToken = botToken;
+        this.botUsername = botUsername;
+        this.webhookPath = webhookPath;
+        this.handlerRouter = new HandlerRouter(this);
+    }
 
-        this.masterHandler = new MasterHandler();
-        this.startHandler = new StartHandler();
+    @Override
+    public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
+        return handlerRouter.route(update);
+    }
+
+    @Override
+    public String getBotPath() {
+        return webhookPath;
     }
 
     @Override
@@ -32,23 +39,5 @@ public class BotMain extends TelegramWebhookBot {
     @Override
     public String getBotToken() {
         return botToken;
-    }
-
-    @Override
-    public String getBotPath() {
-        return botPath;
-    }
-
-    @Override
-    public void onWebhookUpdateReceived(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            String text = update.getMessage().getText();
-
-            if (text.equals("/start")) {
-                startHandler.handleStart(update, this);
-            } else {
-                masterHandler.handle(update, this);
-            }
-        }
     }
 }
