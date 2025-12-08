b5d0a8cc57984870817aae0d5bb084ab4359b4a3
davidmaloney <lesabeillesduvallon@gmail.com>
2025-12-08 18:38:25 +0100

    Update ApplicationMain.java

--- src/main/java/com/yourname/bot/ApplicationMain.java
+++ src/main/java/com/yourname/bot/ApplicationMain.java
@@ -1,12 +1,22 @@
 package com.yourname.bot;
 
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
 public class ApplicationMain {
-
     public static void main(String[] args) {
-        String botToken = System.getenv("TELEGRAM_BOT_TOKEN");
-        String botUsername = System.getenv("TELEGRAM_BOT_USERNAME");
+        String botUsername = System.getenv("BOT_USERNAME");
+        String botToken = System.getenv("BOT_TOKEN");
 
-        BotMain bot = new BotMain(botToken, botUsername);
-        bot.registerBot();
+        BotMain bot = new BotMain(botUsername, botToken);
+
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(bot);
+            System.out.println("Bot started successfully!");
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
