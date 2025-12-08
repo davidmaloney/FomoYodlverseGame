43bd46e49fc007b6a82bc316d6417518841d6a9a
davidmaloney <lesabeillesduvallon@gmail.com>
2025-12-08 17:45:07 +0100

    Update StartHandler.java

--- src/main/java/com/yourname/bot/handlers/StartHandler.java
+++ src/main/java/com/yourname/bot/handlers/StartHandler.java
@@ -3,29 +3,26 @@ package com.yourname.bot.handlers;
 import com.yourname.bot.BotMain;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class StartHandler {
 
-    public static void handleStart(Update update, BotMain bot) {
-        String chatId = update.getMessage().getChatId().toString();
-        String welcomeText = "Welcome to FOMO Euroverse game!";
+    private final BotMain bot;
 
-        // ===== New debug snippet =====
-        System.out.println("=== StartHandler Debug ===");
-        System.out.println("ChatId: " + chatId);
-        System.out.println("Message object: " + update.getMessage());
-        System.out.println("Outgoing text: " + welcomeText);
-        System.out.println("===========================");
-        // =============================
+    public StartHandler(BotMain bot) {
+        this.bot = bot;
+    }
 
-        SendMessage message = new SendMessage();
-        message.setChatId(chatId);
-        message.setText(welcomeText);
-
-        try {
-            bot.execute(message);
-        } catch (Exception e) {
-            e.printStackTrace();
+    public void handle(Update update) {
+        if (update.hasMessage() && update.getMessage().hasChatId()) {
+            SendMessage message = new SendMessage();
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setText("Welcome to FOMO Euroverse game!");  // Main start message
+            try {
+                bot.execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
