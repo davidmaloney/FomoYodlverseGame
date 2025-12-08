539ef9bb8643103acfdc4ff60fb2b9316743e7b6
davidmaloney <lesabeillesduvallon@gmail.com>
2025-12-08 19:59:45 +0100

    Update HandlerRouter.java

--- src/main/java/com/yourname/bot/handlers/HandlerRouter.java
+++ src/main/java/com/yourname/bot/handlers/HandlerRouter.java
@@ -1,22 +1,29 @@
 package com.yourname.bot.handlers;
 
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
+
+import com.yourname.bot.BotMain;
 
 public class HandlerRouter {
 
     private final StartHandler startHandler;
     private final MasterHandler masterHandler;
 
-    public HandlerRouter() {
-        startHandler = new StartHandler();
-        masterHandler = new MasterHandler();
+    public HandlerRouter(BotMain bot) {
+        this.startHandler = new StartHandler(bot);
+        this.masterHandler = new MasterHandler(bot);
     }
 
-    public void route(Update update) {
-        if (update.hasMessage() && update.getMessage().getText().startsWith("/start")) {
-            startHandler.handle(update);
-        } else {
-            masterHandler.handle(update);
+    public BotApiMethod<?> route(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String text = update.getMessage().getText();
+
+            if (text.equals("/start")) {
+                return startHandler.handle(update);
+            }
         }
+
+        return masterHandler.handle(update);
     }
 }
