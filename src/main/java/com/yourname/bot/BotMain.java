4a89d72c255e7d7780ee78f5b3081fe3ed9d17a3
davidmaloney <lesabeillesduvallon@gmail.com>
2025-12-08 18:39:41 +0100

    Update BotMain.java

--- src/main/java/com/yourname/bot/BotMain.java
+++ src/main/java/com/yourname/bot/BotMain.java
@@ -2,6 +2,7 @@ package com.yourname.bot;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class BotMain extends TelegramLongPollingBot {
@@ -15,9 +16,8 @@ public class BotMain extends TelegramLongPollingBot {
     }
 
     @Override
-    public void onUpdateReceived(Update update) {
-        // Route updates to the router
-        Router.handleUpdate(update);
+    public String getBotToken() {
+        return botToken;
     }
 
     @Override
@@ -26,7 +26,20 @@ public class BotMain extends TelegramLongPollingBot {
     }
 
     @Override
-    public String getBotToken() {
-        return botToken;
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage();
+            message.setChatId(String.valueOf(chatId));
+            message.setText("Received: " + messageText);
+
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
