d75ce85b2f08c64d61d502cf4b2a2b9ca3790a42
davidmaloney <lesabeillesduvallon@gmail.com>
2025-12-09 20:41:11 +0100

    Update ExplorationManager.java

--- src/main/java/com/yourname/bot/game/locations/ExplorationManager.java
+++ src/main/java/com/yourname/bot/game/locations/ExplorationManager.java
@@ -1,1 +1,18 @@
+package com.yourname.bot.game.locations;
 
+import java.util.Random;
+
+public class ExplorationManager {
+
+    private static final String[] EVENTS = {
+            "You found a hidden treasure!",
+            "A wild enemy appears!",
+            "Nothing here... just a peaceful path."
+    };
+
+    public static String explore(long chatId) {
+        Random rand = new Random();
+        int idx = rand.nextInt(EVENTS.length);
+        return EVENTS[idx];
+    }
+}
