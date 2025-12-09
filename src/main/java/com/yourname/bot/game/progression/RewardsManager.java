389dbe095531b461ca14b34ef38c12c7f51100d1
davidmaloney <lesabeillesduvallon@gmail.com>
2025-12-09 20:43:44 +0100

    Update RewardsManager.java

--- src/main/java/com/yourname/bot/game/progression/RewardsManager.java
+++ src/main/java/com/yourname/bot/game/progression/RewardsManager.java
@@ -1,1 +1,9 @@
+package com.yourname.bot.game.progression;
 
+public class RewardsManager {
+
+    public static String grantReward(long chatId) {
+        XPManager.addXP(chatId, 20);
+        return "You gained 20 XP! Level: " + XPManager.getLevel(chatId);
+    }
+}
