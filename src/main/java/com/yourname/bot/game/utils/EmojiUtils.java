49645c880524fc1410ca42581ab3bd94e9f47082
davidmaloney <lesabeillesduvallon@gmail.com>
2025-12-09 20:46:26 +0100

    Update EmojiUtils.java

--- src/main/java/com/yourname/bot/game/utils/EmojiUtils.java
+++ src/main/java/com/yourname/bot/game/utils/EmojiUtils.java
@@ -1,1 +1,14 @@
+package com.yourname.bot.utils;
 
+public class EmojiUtils {
+
+    public static String getEmoji(String name) {
+        switch(name.toLowerCase()) {
+            case "sword": return "⚔️";
+            case "shield": return "🛡️";
+            case "potion": return "🧪";
+            case "coin": return "🪙";
+            default: return "";
+        }
+    }
+}
