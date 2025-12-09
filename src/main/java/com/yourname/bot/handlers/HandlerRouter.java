3e58f647bc42a95c64d60116c91c55b908cd31f1
davidmaloney <lesabeillesduvallon@gmail.com>
2025-12-09 20:02:35 +0100

    Update HandlerRouter.java

--- src/main/java/com/yourname/bot/handlers/HandlerRouter.java
+++ src/main/java/com/yourname/bot/handlers/HandlerRouter.java
@@ -2,26 +2,32 @@ package com.yourname.bot.handlers;
 
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
-
-import com.yourname.bot.BotMain;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 
 public class HandlerRouter {
 
     private final StartHandler startHandler;
     private final MasterHandler masterHandler;
+    private final TutorialHandler tutorialHandler;
 
-    public HandlerRouter(BotMain bot) {
-        this.startHandler = new StartHandler(bot);
-        this.masterHandler = new MasterHandler(bot);
+    public HandlerRouter() {
+        this.startHandler = new StartHandler();
+        this.masterHandler = new MasterHandler();
+        this.tutorialHandler = new TutorialHandler();
     }
 
     public BotApiMethod<?> route(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
             String text = update.getMessage().getText();
-
             if (text.equals("/start")) {
-                return startHandler.handle(update);
+                return tutorialHandler.handle(update); // first-time players see tutorial
             }
+            return masterHandler.handle(update);
+        }
+
+        if (update.hasCallbackQuery()) {
+            CallbackQuery callback = update.getCallbackQuery();
+            return masterHandler.handleCallback(callback);
         }
 
         return masterHandler.handle(update);
