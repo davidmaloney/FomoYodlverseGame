89d09de15558ecb6fc3d5a7cbe48ab340f2a0de0
davidmaloney <lesabeillesduvallon@gmail.com>
2025-12-09 20:28:48 +0100

    Update CombatEngine.java

--- src/main/java/com/yourname/bot/game/combat/CombatEngine.java
+++ src/main/java/com/yourname/bot/game/combat/CombatEngine.java
@@ -1,1 +1,21 @@
+package com.yourname.bot.game.combat;
 
+import com.yourname.bot.game.combat.PlayerManager;
+import com.yourname.bot.game.combat.EnemyManager;
+
+public class CombatEngine {
+
+    public static String fightTurn(long chatId) {
+        // Placeholder logic for a fight
+        int playerDamage = PlayerManager.getPlayerDamage(chatId);
+        int enemyDamage = EnemyManager.getEnemyDamage(chatId);
+
+        PlayerManager.dealDamage(chatId, enemyDamage);
+        EnemyManager.dealDamage(chatId, playerDamage);
+
+        return "You dealt " + playerDamage + " damage!\n" +
+               "Enemy dealt " + enemyDamage + " damage!\n" +
+               "Your HP: " + PlayerManager.getPlayerHP(chatId) +
+               "\nEnemy HP: " + EnemyManager.getEnemyHP(chatId);
+    }
+}
