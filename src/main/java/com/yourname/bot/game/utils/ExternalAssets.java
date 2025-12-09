f6f8c00bbe966a4f098b1360efedfe493ca7a98f
davidmaloney <lesabeillesduvallon@gmail.com>
2025-12-09 20:47:32 +0100

    Update ExternalAssets.java

--- src/main/java/com/yourname/bot/game/utils/ExternalAssets.java
+++ src/main/java/com/yourname/bot/game/utils/ExternalAssets.java
@@ -1,1 +1,13 @@
+package com.yourname.bot.utils;
 
+public class ExternalAssets {
+
+    public static String getPlaceholderImage(String type) {
+        switch (type.toLowerCase()) {
+            case "boss": return "https://example.com/images/boss.png";
+            case "enemy": return "https://example.com/images/enemy.png";
+            case "item": return "https://example.com/images/item.png";
+            default: return "https://example.com/images/default.png";
+        }
+    }
+}
